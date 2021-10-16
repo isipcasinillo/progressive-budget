bc896c9c0eba4943c88ff5a66de7ab3e0fc810f2
isipcasinillo <isipcasinillo@gmail.com>
2021-10-15 23:26:12 -0500

    atlas working

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-
+require('dotenv').config
 const PORT = 3000;
 
 const app = express();
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb+srv://admin:admin123@iscluster.icaxk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 // routes
 app.use(require("./routes/api.js"));
